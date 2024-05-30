0f05ed1aec6cb1cc3d025fac687ebea9add2dc46
HaotianXUE <Haotian.Xue@etu.sorbonne-universite.fr>
2024-05-30 16:23:06 +0200

    Gestion de ClickListen pour un bouton et log d'erreur

--- app/src/main/java/com/ndroid/dessert_shop/MainActivity.kt
+++ app/src/main/java/com/ndroid/dessert_shop/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.ndroid.dessert_shop
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +22,32 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val connect = findViewById<Button>(R.id.connect)
+        val email = findViewById<EditText>(R.id.mail)
+        val password = findViewById<EditText>(R.id.password)
+        val error = findViewById<TextView>(R.id.error_password)
+
+
+        connect.setOnClickListener{
+            val textEmail = email.text.toString()
+            val textPassword = password.text.toString()
+            if(textEmail.trim().isEmpty() ||textPassword.trim().isEmpty() ) {
+                error.text = "Vous devez remplir tout les champs!"
+                error.visibility = View.VISIBLE
+            }
+            else {
+                val correctEmail = "admin"
+                val correctPassword = "root1234"
+                if(correctEmail == textEmail && correctPassword == textPassword){
+                    Toast.makeText(this,"connect successful",Toast.LENGTH_LONG).show()
+                }else{
+                    error.text = "Email ou mot de passe est incorrect!"
+                    error.visibility = View.VISIBLE
+                }
+            }
+        }
+
+
+
     }
 }
